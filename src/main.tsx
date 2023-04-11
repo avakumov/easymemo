796f3233884b3d157919c113a659f4ba1da9e625
Vladimir Avakumov <avakdev@gmail.com>
2023-04-11 16:17:16 +0300

    added react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import styled from 'styled-components';
 import App from './components/App/App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QuestionProps } from './components/Question/Question';
-import data from '../data/nvim.json'
+import data from '../data/nvim.json';
 
-
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App questions={data} />,
+	},
+	{
+		path: '/admin',
+		element: <div>admin</div>,
+	},
+]);
 
 const Root = styled.div`
 	margin: 0;
@@ -19,7 +29,7 @@ const Root = styled.div`
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<Root>
-			<App questions={data} />
+			<RouterProvider router={router} />
 		</Root>
 	</React.StrictMode>
 );

bc86a5b401561020a876bf22d8e50a3362f6bedb
Avakumov Vladimir <avakdev@gmail.com>
2023-07-18 10:51:31 +0300

    add user module boilerplate

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PrismaModule } from "./prisma/prisma.module";
 import { QuestionsModule } from "./questions/questions.module";
+import { UsersModule } from "./users/users.module";
 
 @Module({
-  imports: [PrismaModule, QuestionsModule],
+  imports: [PrismaModule, QuestionsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ api/src/test-api.md
@@ -0,0 +1,3 @@
+GET http://localhost:8001
+GET http://localhost:8001/users
+GET http://localhost:8001/questions

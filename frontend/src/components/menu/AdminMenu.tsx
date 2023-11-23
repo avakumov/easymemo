3262d0e876a0a963c200249843994694e2d534af
Avakumov Vladimir <avakdev@gmail.com>
2023-11-23 13:35:40 +0300

    feat: add to menu selected item

--- frontend/src/components/menu/AdminMenu.tsx
+++ frontend/src/components/menu/AdminMenu.tsx
@@ -3,7 +3,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import GroupIcon from '@mui/icons-material/Group';
 import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
 import CategoryIcon from '@mui/icons-material/Category';
@@ -12,17 +12,17 @@ import { EntityNames } from '../../types';
 
 const entities = [
 	{
-		name: EntityNames.USER.toUpperCase(),
+		name: EntityNames.USER,
 		path: `?show=${EntityNames.USER}`,
 		icon: <GroupIcon sx={{ color: 'text.primary' }} />,
 	},
 	{
-		name: EntityNames.QUESTION.toUpperCase(),
+		name: EntityNames.QUESTION,
 		path: `?show=${EntityNames.QUESTION}`,
 		icon: <QuestionMarkIcon sx={{ color: 'text.primary' }} />,
 	},
 	{
-		name: EntityNames.CATEGORY.toUpperCase(),
+		name: EntityNames.CATEGORY,
 		path: `?show=${EntityNames.CATEGORY}`,
 		icon: <CategoryIcon sx={{ color: 'text.primary' }} />,
 	},
@@ -30,15 +30,21 @@ const entities = [
 
 export default function AdminMenu() {
 	const navigate = useNavigate();
+	const [params] = useSearchParams();
+	const name = params.get('show');
+
 	return (
 		<List>
 			{entities.map((item, _index) => (
 				<ListItem key={item.name} disablePadding>
-					<ListItemButton dense onClick={() => navigate(item.path, { replace: false })}>
+					<ListItemButton
+						dense
+						onClick={() => navigate(item.path, { replace: false })}
+						selected={name === item.name}>
 						<ListItemIcon>
 							{item.icon ? item.icon : <TableRowsIcon sx={{ color: 'text.primary' }} />}
 						</ListItemIcon>
-						<ListItemText primary={item.name} />
+						<ListItemText primary={item.name} sx={{ textTransform: 'uppercase' }} />
 					</ListItemButton>
 				</ListItem>
 			))}

ce020520f42cbda4dcfff993ebd25a04ca9ee30a
Avakumov Vladimir <avakdev@gmail.com>
2023-12-19 01:26:15 +0300

    refactor: rewrite practice filter

--- frontend/src/components/filters/PracticeFilter.tsx
+++ frontend/src/components/filters/PracticeFilter.tsx
@@ -1,103 +1,118 @@
-import { useSearchParams } from 'react-router-dom';
 import api from '../../services/ApiService';
-import { map } from '../../utils';
 
-import { SubmitHandler, useForm, Controller } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import ReplayIcon from '@mui/icons-material/Replay';
-import settings from '../../settings';
-import { Box, Button, Checkbox, Input, Sheet } from '@mui/joy';
+import { useDispatch, useSelector } from 'react-redux';
+import { Box, Button, Checkbox, Option, ListItem, Select, Sheet, Typography } from '@mui/joy';
+import { RootState } from '../../store/store';
+import { Controller, useForm } from 'react-hook-form';
+import { closePracticeFilterModal, setPracticeFilter } from '../../store/slices/practiceSlice';
 
-interface ParamsI {
-	count: number;
-	[index: string]: boolean | number;
-}
+type FormType = Record<string, boolean | number>;
 
 export default function PracticeFilter() {
 	const dispatch = useDispatch();
-	const [searchParams, setSearchParams] = useSearchParams();
+	const filter = useSelector((state: RootState) => state.practice.filter);
 	const { data: categories } = api.useGetCategoriesQuery();
 
-	/*По умолчанию выбрана первая категория*/
-	useEffect(() => {
-		const currentCategory = searchParams.get('categories');
-		if (!currentCategory && categories && categories[0]) {
-			setSearchParams({ categories: categories[0].name });
-		}
-	}, [categories, searchParams, setSearchParams]);
+	const cats: Record<string, boolean> = {};
+	const defaultValues = {
+		count: filter.count,
+		...filter.categories.reduce((acc, cur) => {
+			acc[cur as keyof typeof cats] = true;
+			return acc;
+		}, cats),
+	};
 
-	const { handleSubmit, control, watch } = useForm<any>({
-		defaultValues: {
-			count: settings.practice.DEFAULT_TASK_COUNT,
-		},
-	});
+	const { handleSubmit, control, setValue } = useForm<FormType>({ defaultValues });
 
-	/*синхронизировать форму со строкой запроса*/
-	useEffect(() => {
-		const subscription = watch((q) => {
-			//сбрасываем кеш для получения новых заданий
-			dispatch(api.util.invalidateTags(['practice']));
-			//Устанавливам параметры запроса
-			Object.keys(q).forEach((key) => (q[key] === undefined || q[key] === false) && delete q[key]);
-			const paramsCategories = getParamsForRequest(q);
-			setSearchParams({});
-			setSearchParams(paramsCategories);
+	const submit = (formData: FormType) => {
+		const count: number = formData.count as number;
+		delete formData['count'];
+		const categories: string[] = [];
+		Object.keys(formData).forEach((key) => {
+			if (formData[key]) {
+				categories.push(key);
+			}
 		});
-
-		return () => subscription.unsubscribe();
-	}, [dispatch, setSearchParams, watch]);
-
-	function getParamsForRequest(formData: ParamsI) {
-		const categories = getCategories(formData);
-		const count = getCount(formData);
-		return {
-			...(count ? { count } : undefined),
-			...(categories ? { categories } : undefined),
+		const filter = {
+			count,
+			categories,
 		};
-
-		function getCount(formData: ParamsI) {
-			return formData.count.toString() ?? undefined;
-		}
-		function getCategories(formData: ParamsI) {
-			const categories: string[] = [];
-			Object.keys(formData).forEach((key) => {
-				if (formData[key] && key !== 'count') {
-					categories.push(key);
-				}
-			});
-			return categories;
-		}
-	}
-
-	function replayHandler() {
-		dispatch(api.util.invalidateTags(['practice']));
-	}
-
-	const submit: SubmitHandler<any> = async (q) => {
-		//TODO watch time
+		dispatch(setPracticeFilter({ filter }));
+		dispatch(closePracticeFilterModal());
 	};
 
 	return (
-		<Box component='form' noValidate sx={{ mt: 1 }} onSubmit={handleSubmit(submit)}>
-			<Sheet sx={{ display: 'flex', alignItem: 'center', justifyContent: 'space-evenly', gap: 1, p: 1 }}>
-				{map(categories, (c) => (
-					<Controller
-						key={c.id}
-						name={c.name}
-						control={control}
-						render={({ field }) => <Checkbox label={c.name} {...field} />}
-					/>
-				))}
+		<Sheet sx={{ m: 1, width: '400px' }} component='form' onSubmit={handleSubmit(submit)}>
+			<Typography level='title-lg' sx={{ mb: '2rem', textAlign: 'center' }}>
+				Filter on practice
+			</Typography>
+			<Typography level='body-lg' sx={{ mb: '2px', pl: '1rem' }}>
+				Categories
+			</Typography>
+			<Sheet variant='outlined' sx={{ borderRadius: '1rem', mb: '2rem', p: '0.5rem' }}>
+				<Box
+					sx={{
+						display: 'flex',
+						gap: '0.5rem',
+						flexWrap: 'wrap',
+						justifyContent: 'center',
+						'--ListItem-radius': '1rem',
+					}}>
+					{Array.isArray(categories) &&
+						categories.map((item) => (
+							<ListItem key={item.id} sx={{ py: '0.5rem', px: '1rem' }}>
+								<Controller
+									name={item.name}
+									control={control}
+									render={({ field }) => (
+										<Checkbox
+											{...field}
+											overlay
+											disableIcon
+											variant='soft'
+											label={item.name}
+											checked={field.value ? true : false}
+											value={field.value ? '1' : '0'}
+										/>
+									)}
+								/>
+							</ListItem>
+						))}
+				</Box>
+			</Sheet>
+
+			<Typography level='body-lg' sx={{ mb: '2px', pl: '1rem' }}>
+				Count
+			</Typography>
+			<Sheet variant='outlined' sx={{ borderRadius: '1rem', mb: '2rem', p: '0.5rem' }}>
 				<Controller
 					name='count'
 					control={control}
-					render={({ field }) => <Input type='number' sx={{ width: '70px' }} {...field} />}
+					render={({ field }) => (
+						<Select
+							variant='soft'
+							{...field}
+							value={field.value ? field.value : undefined}
+							onChange={(_, newValue) => {
+								newValue && setValue('count', newValue);
+							}}>
+							{[10, 50, 100].map((count) => (
+								<Option key={count} value={count}>
+									{count}
+								</Option>
+							))}
+						</Select>
+					)}
 				/>
-				<Button variant='soft' onClick={replayHandler} startDecorator={<ReplayIcon />}>
-					update
-				</Button>
 			</Sheet>
-		</Box>
+			<Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+				<Button variant='soft' onClick={() => dispatch(closePracticeFilterModal())}>
+					Cancel
+				</Button>
+				<Button variant='soft' type='submit'>
+					Ok
+				</Button>
+			</Box>
+		</Sheet>
 	);
 }

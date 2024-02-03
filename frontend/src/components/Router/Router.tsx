483812bed834460b74800fc0eb12bb5f8217c6e6
Avakumov Vladimir <avakdev@gmail.com>
2024-02-03 13:38:05 +0300

    deploy: remove prefix

--- frontend/src/components/Router/Router.tsx
+++ frontend/src/components/Router/Router.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
-import settings from '../../settings';
 import Error404 from '../Error404/Error404';
 import MainAuthLayout from '../layouts/MainAuthLayout';
 import PracticeFilterModal from '../modals/PracticeFilterModal';
@@ -7,41 +6,36 @@ import Practice from '../Practice/Practice';
 import Records from '../Records/Records';
 import Registration from '../Registration/Registration';
 
-const router = createBrowserRouter(
-	[
-		{
-			path: '/',
-			element: (
-				<MainAuthLayout>
-					<Practice />
-					<PracticeFilterModal />
-				</MainAuthLayout>
-			),
-		},
-		{
-			path: '/records',
-			element: (
-				<MainAuthLayout>
-					<Records />
-				</MainAuthLayout>
-			),
-		},
-		{
-			path: '/stats',
-			element: <MainAuthLayout>stats</MainAuthLayout>,
-		},
-		{
-			path: '/registration',
-			element: <Registration />,
-		},
-		{
-			path: '*',
-			element: <Error404 />,
-		},
-	],
+const router = createBrowserRouter([
 	{
-		basename: settings.URL_PREFIX,
-	}
-);
+		path: '/',
+		element: (
+			<MainAuthLayout>
+				<Practice />
+				<PracticeFilterModal />
+			</MainAuthLayout>
+		),
+	},
+	{
+		path: '/records',
+		element: (
+			<MainAuthLayout>
+				<Records />
+			</MainAuthLayout>
+		),
+	},
+	{
+		path: '/stats',
+		element: <MainAuthLayout>stats</MainAuthLayout>,
+	},
+	{
+		path: '/registration',
+		element: <Registration />,
+	},
+	{
+		path: '*',
+		element: <Error404 />,
+	},
+]);
 
 export default router;

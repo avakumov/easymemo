6aba82a41197d927cfb0d3c7503a3ec9fb7c1728
Avakumov Vladimir <avakdev@gmail.com>
2024-02-18 19:10:48 +0300

    feat: add to upload mp3 file

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,20 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { QuestionsModule } from './questions/questions.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ConfigModule } from '@nestjs/config';
+import { FilesModule } from './files/files.module';
 import mailerConfig from './config/mailer.config';
 import appConfig from './config/app.config';
+import authConfig from './config/auth.config';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-			load: [mailerConfig, appConfig],
+			load: [mailerConfig, appConfig, authConfig],
 			envFilePath: ['.env'],
 		}),
 		PrismaModule,
@@ -22,8 +22,9 @@ import appConfig from './config/app.config';
 		UsersModule,
 		AuthModule,
 		CategoriesModule,
+		FilesModule,
 	],
-	controllers: [AppController],
-	providers: [AppService],
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
---
+++ api/src/files/files.controller.ts
@@ -0,0 +1,62 @@
+import {
+	Controller,
+	Delete,
+	Get,
+	Param,
+	ParseFilePipeBuilder,
+	Post,
+	UploadedFile,
+	UseInterceptors,
+	StreamableFile,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { createReadStream, unlink } from 'node:fs';
+import { v4 as uuidv4 } from 'uuid';
+
+const path_to_audio = './uploads/audio';
+
+@Controller('files')
+export class FilesController {
+	@UseInterceptors(
+		FileInterceptor('file', {
+			storage: diskStorage({
+				destination: path_to_audio,
+				filename: (req, file, callback) => {
+					const fileExtName = file.originalname.split('.').pop();
+					const randomName = uuidv4();
+					callback(null, `${randomName}.${fileExtName}`);
+				},
+			}),
+		})
+	)
+	@Post('upload/mp3')
+	uploadFileAndFailValidation(
+		@UploadedFile(
+			new ParseFilePipeBuilder()
+				.addFileTypeValidator({
+					fileType: 'audio/mpeg',
+				})
+				.build()
+		)
+		@UploadedFile()
+		file: Express.Multer.File
+	) {
+		return {
+			file,
+		};
+	}
+
+	@Delete('remove/:filename')
+	removeFile(@Param('filename') name: string) {
+		unlink(path_to_audio + '/' + name, (error) => {
+			console.log('error: ', error);
+		});
+	}
+
+	@Get('get/:filename')
+	getFlile(@Param('filename') filename: string): StreamableFile {
+		const file = createReadStream(path_to_audio + '/' + filename);
+		return new StreamableFile(file);
+	}
+}
